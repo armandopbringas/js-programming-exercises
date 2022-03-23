c0855dfb34802001e93c509d6dd7ebcdec889a64
armanopbringas <bringas.armandop@gmail.com>
2022-03-23 14:32:40 -0600

    adding findIndex example

--- array-methods/find/find.js
+++ array-methods/find/find.js
@@ -11,3 +11,31 @@ console.log(rta);
 const findNum = numbers.find(item => item === 30);
 
 console.log(findNum);
+
+const products = [
+  {
+    name: "Pizza",
+    price: 12,
+    id: '🍕'
+  },
+  {
+    name: "Burger",
+    price: 23,
+    id: '🍔'
+  },
+  {
+    name: "Hot dog",
+    price: 34,
+    id: '🌭'
+  },
+  {
+    name: "Hot cakes",
+    price: 355,
+    id: '🥞'
+  },
+];
+
+const findBurguerEl = products.find(item => item.id === '🍔')
+console.log('find', findBurguerEl);
+const findIndexEl = products.findIndex(item => item.id === '🍔')
+console.log('findIndex', findIndexEl);
