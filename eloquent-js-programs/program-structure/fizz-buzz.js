8275b1d2eafd8ff2b316d16be071b81f78c51542
Armando Bringas <bringas.armandop@gmail.com>
2023-08-10 23:13:03 -0600

    Update fizz-buzz.js

--- eloquent-js-programs/program-structure/fizz-buzz.js
+++ eloquent-js-programs/program-structure/fizz-buzz.js
@@ -2,7 +2,7 @@ const nums = Array.from({lenght: 30}, (_, i + 1));
 
 const fizzBuzz = () => {
 	nums.forEach(num => {
-		if (i % 3 =?= 0 && i % 5 == 0) return console.log("FizzBuzz");
+		if (i % 3 === 0 && i % 5 == 0) return console.log("FizzBuzz");
 		if (i % 3 === 0) return console.log("Fizz");
 		if (i % 5 === 0) return console.log("Buzz");
 		return console.log(num);
